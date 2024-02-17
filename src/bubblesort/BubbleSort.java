0ee803919357de58979e821e342bca1b4cc4ee1a
Chamudidj <160303971+Chamudidj@users.noreply.github.com>
2024-02-17 11:50:02 +0530

    Initial commit

---
+++ src/bubblesort/BubbleSort.java
@@ -0,0 +1,32 @@
+package bubblesort;
+
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        
+        int array[]={90,80,70,60,50,40};
+        int temp;
+        int n=array.length;
+        
+        for(int phrase=0;phrase<(n-1);phrase++)
+        {
+            for(int round=0;round<((n-phrase)-1);round++)
+            {
+                if(array[round]>array[round+1])
+                {
+                    temp=array[round];
+                    array[round]=array[round+1];
+                    array[round+1]=temp;
+                }
+            }
+        }
+        
+        for(int k=0;k<array.length;k++)
+        {
+            System.out.println(" "+array[k]);
+        }
+        
+    }
+    
+}
